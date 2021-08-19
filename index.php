1c3b98dc1dce07fc867f90f29b9ead8d41f03ef0
maksim ezikov <ezikov.maxim@gmail.com>
2021-08-19 16:02:54 +0300

    add versions

--- index.php
+++ index.php
@@ -3,6 +3,6 @@ $ip_server = $_SERVER['SERVER_ADDR'];
 
 echo "<h1>Hello from Kubernetes</h1><br>";
 echo "Server IP Address is: $ip_server", "<br><p>";
-echo "Made by <font color=blue>Maksim Ezikov";
+echo "Made by <font color=blue>Maksim Ezikov version3";
 
 ?>
